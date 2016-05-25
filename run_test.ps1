01db95d8c61e653989927720a677c5fc2a2895ff
GregBestland <Greg.Bestland@datastax.com>
2016-05-25 11:38:54 -0500

    Add more debug trying explicit nosetest path

--- run_test.ps1
+++ run_test.ps1
@@ -1,7 +1,6 @@
 Set-ExecutionPolicy Unrestricted
 Get-ExecutionPolicy
 echo $env:Path
-powershell.exe -executionpolicy Unrestricted C:\Python27\Scripts\nosetests.exe -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard\test_cluster.py:ClusterTests
 echo "uploading results"
 echo $env:APPVEYOR_JOB_ID
 $wc = New-Object 'System.Net.WebClient'
