0727b43783c992b752a592c138f1cba9e89ea8f8
GregBestland <Greg.Bestland@datastax.com>
2016-05-25 12:07:44 -0500

    Using Start-Process instead of powershell invocation

--- run_test.ps1
+++ run_test.ps1
@@ -1,6 +1,8 @@
 Set-ExecutionPolicy Unrestricted
-Get-ExecutionPolicy
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -force
+Get-ExecutionPolicy -List
 echo $env:Path
+Start-Process nosetests -ArgumentList "-s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard\test_cluster.py:ClusterTests" -Wait -NoNewWindow -PassThru
 echo "uploading results"
 echo $env:APPVEYOR_JOB_ID
 $wc = New-Object 'System.Net.WebClient'
