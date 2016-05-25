033c91e5e044e86b983dd983a1a8545f8b6ac68d
GregBestland <Greg.Bestland@datastax.com>
2016-05-25 13:04:59 -0500

    Futher Tweaking Execution Policies

--- run_test.ps1
+++ run_test.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -force
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -force
 Get-ExecutionPolicy -List
 echo $env:Path
-Start-Process nosetests -ArgumentList "-s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard\test_cluster.py:ClusterTests" -Wait -NoNewWindow -PassThru
+nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard\test_cluster.py:ClusterTests
 echo "uploading results"
 echo $env:APPVEYOR_JOB_ID
 $wc = New-Object 'System.Net.WebClient'
