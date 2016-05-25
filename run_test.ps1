63440abb24900d2146cfaf8562e5a2e7ae45290f
GregBestland <Greg.Bestland@datastax.com>
2016-05-25 16:38:51 -0500

    more debug

--- run_test.ps1
+++ run_test.ps1
@@ -4,7 +4,8 @@ Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -force
 Get-ExecutionPolicy -List
 echo $env:Path
 echo $env:JAVA_HOME
-echo $env;PYTHONPATH
+echo $env:PYTHONPATH
+echo $env:CASSANDRA_VERSION
 python --version
 python -c "import platform; print(platform.architecture())"
 nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard\test_cluster.py:ClusterTests
