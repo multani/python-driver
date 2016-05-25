78fabb8e4c7e9331d44a9324cf9d802f2111866a
GregBestland <Greg.Bestland@datastax.com>
2016-05-25 09:45:00 -0500

    Simplifying standard tests for troubleshooting purposes

--- run_test.ps1
+++ run_test.ps1
@@ -1,9 +1,7 @@
 Get-ExecutionPolicy
-powershell.exe -executionpolicy Unrestricted nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=unit_results.xml .\tests\unit
-powershell.exe -executionpolicy Unrestricted nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard
+powershell.exe -executionpolicy Unrestricted nosetests -s -v --with-ignore-docstrings --with-xunit --xunit-file=standard_results.xml .\tests\integration\standard\test_cluster:ClusterTests
 echo "uploading results"
 echo $env:APPVEYOR_JOB_ID
 $wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\unit_results.xml))
 $wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\standard_results.xml))
 exit 0
